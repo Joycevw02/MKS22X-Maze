96d13fa52d39f6a1d4743ade047f0dc4ab4b22b9
joycevw02 <jwang01@stuy.edu>
2019-02-26 14:52:47 -0500

    Started array

--- Readfile.java
+++ Readfile.java
@@ -2,12 +2,21 @@ import java.io.*;
 import java.util.*;
 public class Readfile{
   public static void main(String args[]){
+    int[][] maze;
+    int row = 0;
+    int col = 0;
     try{
       File text = new File("Maze1.txt");
       Scanner s = new Scanner(text);
       while (s.hasNextLine()){
         String temp = s.nextLine();
         System.out.println(temp);
+      }
+      while (s.hasNextLine()){
+        String temp = s.nextLine();
+        for (int i = 0; i < temp.length(); i ++){
+          
+        }
       }
     }
     catch (FileNotFoundException e){
