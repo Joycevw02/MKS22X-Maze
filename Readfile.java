39e47b85d75d269f41c052f442fd37ea15d6a4f0
joycevw02 <jwang01@stuy.edu>
2019-02-26 14:49:36 -0500

    Successfully read and printed out the file

--- Readfile.java
+++ Readfile.java
@@ -1,3 +1,17 @@
+import java.io.*;
+import java.util.*;
 public class Readfile{
-
+  public static void main(String args[]){
+    try{
+      File text = new File("Maze1.txt");
+      Scanner s = new Scanner(text);
+      while (s.hasNextLine()){
+        String temp = s.nextLine();
+        System.out.println(temp);
+      }
+    }
+    catch (FileNotFoundException e){
+      System.out.println("File not found");
+    }
+  }
 }
